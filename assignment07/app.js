c35a04795eb640d7d8eadd75dd84d73f4c325c84
muhammad arsalan <54612901+arsalan414@users.noreply.github.com>
2020-06-25 14:54:50 +0500

    added code

--- assignment07/app.js
+++ assignment07/app.js
@@ -118,3 +118,55 @@ document.write(document.write("<br><br><br>", user_input1[user_input1.length - 1
 //Quetion No.18
 var string2 = "The quick brown fox jumps over the lazy dog";
 document.write("<br><br>There are ", string2.match(/the/gi).length, " of the world 'the'");
+
+
+//chapter 26-30
+
+//question no.01
+var integer = prompt("Enter positive integer");
+document.write("<br><br>Number : ", integer);
+document.write("<br><br>Round off Value : ", Math.round(integer));
+document.write("<br><br>Floor Value : ", Math.floor(integer));
+document.write("<br><br>Ceil Value : ", Math.floor(integer) + 1);
+
+
+//question no.02
+var negative_integer = prompt("Enter positive negative_integer");
+document.write("<br><br>Number : ", negative_integer);
+document.write("<br><br>Round off Value : ", Math.round(negative_integer));
+document.write("<br><br>Floor Value : ", Math.floor(negative_integer));
+document.write("<br><br>Ceil Value : ", Math.floor(negative_integer) + 1);
+
+//question no.03
+var absolute_value = prompt("Enter absolute number");
+document.write("<br><br>The absolute value of ", absolute_value, " is ", Math.abs(absolute_value));
+
+
+//question no.04
+document.write("<br><br>Random Dice Value : ", Math.floor(Math.random() * 6) + 1);
+
+
+//question no.05
+let coin_toss = Math.floor(Math.random() * 2 + 1);
+if (coin_toss == 2) { document.write("<br><br>2<br>RAndom coin value : Heads") }
+else { document.write("<br><br>1<br>Random coin value : Tails"); }
+
+//question no.06
+document.write("<br><br>Random number between 1 and 100 is ", Math.floor(Math.random() * 100 + 1));
+
+//question no.07
+var weight = prompt("Enter weight:");
+let w = weight.match(/[0-9 .]*/);
+document.write('THe weight of user is ', w[0], ' kilograms');
+
+
+//question no.08
+var secret = prompt('Enter secret Number ?');
+var random = Math.random() * 10 + 1;
+
+if (secret == random) {
+    alert("Congratulations! Matched");
+}
+else {
+    alert("Try Again!");
+}
